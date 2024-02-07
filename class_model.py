16a454d3842e3eac8c405dfef08ecfe67e451404
Rohan Awhad <rohanavad007@gmail.com>
2024-02-07 09:36:17 -0700

    inital commit; creating a separate repo for easy usability

---
+++ class_model.py
@@ -0,0 +1,185 @@
+
+
+import asyncio
+from typing import Any, Dict, List, Optional, Union
+import aiohttp
+from loguru import logger
+from pydantic import BaseModel
+import requests
+from tqdm import tqdm
+
+
+class LLMParams(BaseModel):
+  max_tokens: int
+  temperature: float
+  top_p: float
+  top_k: int
+  stop: List[str]
+
+
+
+class LLM:
+  def __init__(
+    self,
+    url: str,
+    api_key: str,
+    model: str,
+    llm_params: LLMParams,
+    prompt_template: str,
+    session: Optional[aiohttp.ClientSession]=None,
+  ):
+    self.url = url
+    self.api_key = api_key
+    self.model = model
+    self.llm_params = llm_params
+    self.prompt_template = prompt_template
+
+    if session is None:
+      self.generate = self._generate
+    else:
+      self.session = session
+      self.generate = self._agenerate
+
+  def __str__(self) -> str:
+    return f"LLM: {self.model}. URL: {self.url}"
+
+  async def _agenerate(self, inputs: Dict[str, Any]) -> str:
+    data = self._prep_json(inputs)
+    headers = {
+      "accept": "application/json",
+      "content-type": "application/json",
+      "Authorization": "Bearer " + self.api_key
+    }
+    async with self.session.post(self.url, json=data, headers=headers) as response:
+      self.response = response
+      return response
+
+  def _generate(self, inputs: Dict[str, Any], verbose=False) -> str:
+    data = self._prep_json(inputs)
+    if verbose:
+      print('LLM Input>')
+      if self.is_openai(): print(data['messages'][0]['content'])
+      else: print(data['prompt'])
+
+    headers = {
+      "accept": "application/json",
+      "content-type": "application/json",
+      "Authorization": "Bearer " + self.api_key
+    }
+    res = requests.post(self.url, json=data, headers=headers)
+    res.status = res.status_code
+    self.response = res
+    return res
+
+  def _prep_json(self, inputs: Dict[str, Any]) -> Dict[str, Any]:
+    data = {
+      "model": self.model,
+      "n": 1,
+      **self.llm_params.model_dump()
+    }
+    if self.is_openai():
+      data['messages'] = [{'role': 'user', 'content': self.prompt_template.format(**inputs)}]
+      data.pop('top_k')
+    else:
+      data['prompt'] = self.prompt_template.format(**inputs)
+    return data
+  
+  def is_openai(self) -> bool:
+    return 'openai' in self.url
+
+  def get_text_from_response(self) -> str:
+    if self.is_openai():
+      return self.response.json()['choices'][0]['message']['content']
+    return self.response.json()['choices'][0]['text']
+
+class GenerationMaster:
+  def __init__(
+    self,
+    llms: Union[LLM, List[LLM]],
+    llm_inputs: List[Dict[str, Any]],
+    num_workers: int,
+    session: aiohttp.ClientSession,
+    max_retries: int,
+  ):
+
+    # validate inputs
+    if isinstance(llms, list):
+      for llm in llms:
+        if not isinstance(llm, LLM):
+          raise ValueError("llms should be a list of LLM objects.")
+    elif not isinstance(llms, LLM):
+      raise ValueError("llms should be a LLM object.")
+
+    if not isinstance(num_workers, int): raise ValueError("num_workers should be an integer.")
+    if not isinstance(session, aiohttp.ClientSession): raise ValueError("session should be an aiohttp.ClientSession object.")
+    if not isinstance(max_retries, int): raise ValueError("max_retries should be an integer.")
+
+    # set attributes
+    self.llms = llms if isinstance(llms, list) else [llms]
+    self.llm_inputs = llm_inputs
+    self.num_workers = min(num_workers, len(llm_inputs))
+    self.session = session
+    self.max_retries = max_retries
+
+    # private attributes
+    self._responses = []
+    self._todo = asyncio.Queue()
+    self._pbar = tqdm(total=len(llm_inputs), desc="Generating", leave=False)
+
+
+  async def run(self) -> List[str]:
+    outputs = []
+    for inputs in self.llm_inputs: await self._todo.put(inputs)
+
+    workers = [asyncio.create_task(self._worker()) for _ in range(self.num_workers)]
+    await self._todo.join()
+    for w in workers: w.cancel()
+    
+    for res in self._responses: outputs.append(res)
+    return outputs
+
+  async def _worker(self):
+    while True:
+      try: await self.process_one()
+      except asyncio.CancelledError: return
+
+  async def process_one(self):
+    inputs = await self._todo.get()
+    res = None
+    raised_exc = None
+    for _ in range(self.max_retries):
+      for inp, llm in zip(inputs, self.llms):
+        try:
+          res = llm.generate(inp)
+          if res.status == 200:
+            res = res.json()
+            self._responses.append(llm.get_text_from_response())
+            self._pbar.update(1)
+            self._todo.task_done()
+            return
+          else:
+            raised_exc = res.text
+            logger.warning(f'Failed to generate text using {llm}. Got status: {res.status}. Text: {res.text}')
+
+        except aiohttp.ClientError as e:
+          raised_exc = e
+          logger.error(f'Client Error: {e}')
+        except asyncio.TimeoutError as e:
+          raised_exc = e
+          logger.error(f'Timeout Error: {e}')
+        except requests.JSONDecodeError as e:
+          raised_exc = e
+          logger.error(f'JSONDecodeError: {e} Got text: {res.text}')
+        except Exception as e:
+          raised_exc = e
+          logger.error(f'Exception: {e}')
+
+
+    self._pbar.update(1)
+    self._todo.task_done()
+
+    # code might not ever reach this
+    if res is None: raise Exception(f"Got exception: {raised_exc}. Failed to generate text for inputs: {inputs}")
+    else: self._responses.append(res)
+
+
---
+++ service.py
@@ -0,0 +1,158 @@
+'''
+Designing an API request service to call LLM providers' APIs.
+
+- The goal of this code it to ALWAYS generate text for a given prompt, as fast as possible.
+
+Requirements:
+- The service needs to make asynchronous requests to the LLM provider's API.
+- If an API request fails, the service should retry the request
+- It should fall back to another LLM provider if the first one fails.
+- Retry
+- Needs to generate a response within a specified time limit, if it cannot,
+  redundant requests should be made to other LLM providers.
+
+Needs:
+- 2 LLM providers and 2 models each for redundancy. (Mixtral & Llama + Gpt-3.5 & Gpt-4)
+
+===
+Based on the above description, I feel following classes/functions should be present
+===
+
+* LLMParams: All the parameters required to change model's sampling behaviour.
+  - max_tokens: number
+  - temperature: decimal
+  - top_p: decimal
+  - top_k: number
+  - stop_tokens: List[str]
+
+* LLM: All the details required to make a request to an LLM provider's API, along with the request method.
+  - url: string
+  - api_key: string
+  - model: string
+  - session: Optional
+  - llm_params: LLMParams
+  + _agenerate: async function
+  + _generate: function which calls the API with normal requests
+  + __call__: based on if a session is present, it should call _agenerate or _generate
+
+* GenerationMaster: All the details required to get generations fast.
+  - llms: List[LLM]  # list of redundancy
+  - input_text_list: List[str]
+  - num_workers: number
+  - session: asyncio.ClientSession with timeout
+  - max_retries: number
+  - _responses: List[str]
+  - _todo: queue of tasks  # asyncio.Queue
+  - _pbar: progress bar  # tqdm
+  + run: async function to run the generation
+  + _worker: async function to process one task and quit when (helper function)
+  + _process_one: async function to generate text and retry if fails
+
+'''
+
+
+import aiohttp
+import asyncio
+import os
+from typing import Any, Dict, List
+
+from .class_model import LLM, GenerationMaster
+
+# together ai config
+url = "https://api.together.xyz/v1/completions"
+api_key = os.environ['TOGETHER_API_KEY']
+NUM_WORKERS = int(os.environ.get('TOGETHER_NUM_WORKERS', 2))
+
+async def _generate(
+  inputs: List[List[Dict[str, Any]]],
+  llm: List[LLM],
+  max_retries: int,
+  total_timeout: int,
+) -> List[str]:
+  '''Helper function to create a session, and run the GenerationMaster.'''
+
+  timeout = aiohttp.ClientTimeout(total=total_timeout)
+  async with aiohttp.ClientSession(timeout=timeout) as session:
+    generator_instance = GenerationMaster(
+      llms=llm,
+      llm_inputs=inputs,
+      num_workers=NUM_WORKERS,
+      session=session,
+      max_retries=max_retries,
+    )
+    return await generator_instance.run()
+
+def generate(
+  inputs: List[List[Dict[str, Any]]],
+  llm: List[LLM],
+  max_retries: int,
+  total_timeout: int,
+) -> List[str]:
+
+  # validate
+  # inputs
+  if not isinstance(inputs, list): raise ValueError('inputs should be a list of list of dictionaries.')
+  for inp in inputs:
+    if not isinstance(inp, list): raise ValueError('inputs should be a list of list of dictionaries.')
+  inp_keys = []
+  for inp in inputs[0]:
+    if not isinstance(inp, dict): raise ValueError('inputs should be a list of list of dictionaries.')
+    inp_keys.append(set(inp.keys()))
+
+  # llms
+  if not isinstance(llm, list): raise ValueError('llm should be a list of LLM objects.')
+  for l in llm:
+    if not isinstance(l, LLM): raise ValueError('llm should be a list of LLM objects.')
+
+  # inputs and llms
+  for inp in inputs:
+    assert len(inp) == len(llm), 'Length of inputs and llms should be same, because llms may have different prompt templates.'
+  for inp in inputs[1:]:
+    for i, x in enumerate(inp):
+      if not isinstance(x, dict): raise ValueError('inputs should be a list of list of dictionaries.')
+      assert set(x.keys()) == inp_keys[i], 'All inputs for a particular llm should have same keys.'
+
+
+  # run the generation
+  return asyncio.run(_generate(
+    inputs,
+    llm=llm,
+    max_retries=max_retries,
+    total_timeout=total_timeout,
+  ))
+
+if __name__ == '__main__':
+  # testing service
+  from class_model import LLMParams
+  llm_params = LLMParams(
+    max_tokens=1024,
+    temperature=0.7,
+    top_p=0.7,
+    top_k=50,
+    stop_tokens=['Human:', '[/INST]', '</s>'],
+  )
+  llms = [
+    LLM(
+      url,
+      api_key,
+      'something',
+      llm_params,
+      '<s>[INST] {query} [/INST]'
+    ),
+    LLM(
+      url,
+      api_key,
+      'mistralai/Mistral-7B-Instruct-v0.2',
+      llm_params,
+      '<s>[INST] {query} [/INST]'
+    ),
+  ]
+  inputs = [
+    {'query': 'What is the capital of France?'},
+  ]
+  print(
+    '\n---\nFinal answer:',
+    generate(inputs, llms, 3, 20)[0]
+  )
+  exit()
+
